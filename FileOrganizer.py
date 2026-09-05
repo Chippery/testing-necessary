5dbbf3fa058b9cc699e77b20026ab74487476c6c
Chip <Chippery7@gmail.com>
2026-09-05 02:37:42 -0400

    Works so far...

--- FileOrganizer.py
+++ FileOrganizer.py
@@ -6,17 +6,15 @@ file_Types = {
     ".mp4" : "Videos",
     ".gif" : "Gifs",
     ".txt" : "Text",
-    ".exe" : "Executable"
+    ".exe" : "Executable",
+    ".ini" : "Initialization",
+    ".java" : "Java_File",
+    ".jpeg" : "Images"
 }
 
 create_folder_path = r'c:\Users\chipp_hqhjylc\Downloads'
-target_dir = r"c:\Users\chipp_hqhjylc\Downloads"
-dir_list = os.listdir(target_dir)
 
-dir_keys = list(file_Types.keys())
-
-list_of_files = ["Code.jpg", "Ex.mp4", "Youtube.txt"]
-
+list_of_files = ["Code.jpg", "Ex.mp4", "Youtube.txt"] # List of example files to sort
 
 for i in list_of_files: # Creates files in downloads path
     with open(os.path.join(create_folder_path, i), "w") as file:
@@ -33,12 +31,20 @@ except FileExistsError as e:
 create_folder_path = r'c:\Users\chipp_hqhjylc\Downloads'
 
 listdir = os.listdir(create_folder_path)
+file_list = []
 
-example_src = (create_folder_path + "\Code.jpg")
-example_dst = (create_folder_path + "\Images")
+for dir in listdir:
+    if os.path.isfile(fr"{create_folder_path}\{dir}") and dir not in file_Types.values():
+        file_list.append(dir)
+print(file_list)
 
-for x in listdir:
-    if '.jpg' in x:
-        print(x)
+example_dst = (create_folder_path + r"\Images")
 
-shutil.move(example_src, example_dst) # Move src to dst
+for eachFile in file_list: # Iterate through example files to sort
+    example_src = (fr"{create_folder_path}\{eachFile}") # src = path + file name
+    example_dst = (fr"{create_folder_path}\{file_Types[os.path.splitext(eachFile)[1]]}")
+
+    try:
+        shutil.move(example_src, example_dst) # Move src to dst
+    except shutil.Error as e:
+        print("Some stupid file already exists, can't move it.") # Can't and Won't move if file already exists
