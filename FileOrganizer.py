79089d49c146cb9fa9359f2e9972e3d6f257c0d0
Chip <Chippery7@gmail.com>
2026-09-08 12:01:14 -0400

    Able to add new file types not already initialized

--- FileOrganizer.py
+++ FileOrganizer.py
@@ -22,7 +22,6 @@ for i in file_Types:
     create_folder_path = (fr"{create_folder_path}\{file_Types[i]}")
     os.makedirs(create_folder_path, exist_ok=True)
     create_folder_path = r'c:\Users\chipp_hqhjylc\Downloads'
-
 create_folder_path = r'c:\Users\chipp_hqhjylc\Downloads' # Path you want to clean up
 
 listdir = os.listdir(create_folder_path)
@@ -31,13 +30,15 @@ file_list = []
 for dir in listdir:
     if os.path.isfile(fr"{create_folder_path}\{dir}") and dir not in file_Types.values():
         file_list.append(dir)
-print(file_list)
-
-example_dst = (create_folder_path + r"\Images")
 
 for eachFile in file_list: # Iterate through example files to sort
+    if os.path.splitext(eachFile)[1] not in file_Types.keys():
+        file_Types.update({os.path.splitext(eachFile)[1] : os.path.splitext(eachFile)[1][1:].capitalize()}) # Add unrecognize file type to dict 
+        os.makedirs(fr"{create_folder_path}\{os.path.splitext(eachFile)[1][1:].capitalize()}", exist_ok=True) # Make folder of unrecognized file type
+        print(f'''Make new folder "{file_Types[os.path.splitext(eachFile)[1]]}"''')
+
     example_src = (fr"{create_folder_path}\{eachFile}") # src = path + file name
-    example_dst = (fr"{create_folder_path}\{file_Types[os.path.splitext(eachFile)[1]]}")
+    example_dst = (fr"{create_folder_path}\{file_Types[os.path.splitext(eachFile)[1]]}") # dst = path + folder name
 
     try:
         shutil.move(example_src, example_dst) # Move src to dst
