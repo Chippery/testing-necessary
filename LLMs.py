f78ad2fdf1a43756d85bdc0df3945f517e7b6bb9
Chip <Chippery7@gmail.com>
2026-09-10 16:09:45 -0400

    Clean up

--- LLMs.py
+++ LLMs.py
@@ -1,13 +1,25 @@
 from openai import OpenAI
-key = "alv2_FkaZPlnFMxMX32BCVuVtxMe1nv_ssd5ewju8Ge6z4pg"
+from pathlib import Path
+import json
+
+with open("key.json", "r") as file:
+    data = json.load(file)
+
 client = OpenAI(
     base_url="https://api.aionlabs.ai/v1",
-    api_key=key)
+    api_key=data["key"])
 
 
+folder_path = r'c:\Users\chipp_hqhjylc\Downloads'
+folder = Path(folder_path)
+
+#for file in folder.iterdir():
+#    print(file)
+
+    
 response = client.chat.completions.create(
     model="aion-labs/aion-2.0",
-    messages=[{"role": "user", "content": "Explain quantum physics simply."}],
+    messages=[{"role": "user", "content": f""}],
     temperature=0.7 # Using Aion, need to pass temperature variable
 )
 print(response.choices[0].message.content)
