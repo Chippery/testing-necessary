c82b5672b0731609241a1bf4fbd419807104ff84
Chip <Chippery7@gmail.com>
2026-09-11 01:13:13 -0400

    Clean up

--- LLMs.py
+++ LLMs.py
@@ -14,23 +14,28 @@ create_folder_path = r'c:\Users\chipp_hqhjylc\Downloads'
 listdir = os.listdir(create_folder_path)
 file_list = []
 
-for eachFile in listdir:
+for eachFile in listdir: # Add files not in directory into list to sort later
     if os.path.isfile(fr"{create_folder_path}\{eachFile}"):
         file_list.append(eachFile)
 print(file_list)
 
-client = OpenAI(
-    base_url="https://api.groq.com/openai/v1", # base_url will be your ai's url
-    api_key=data["key"] # Key will be your api's key
-    )
+listNum = [0, 3, 4, 1, 5]
+
+for index, item in enumerate(file_list):
+    print(folderNames["categories"][listNum[index]]["name"])
+
+# client = OpenAI(
+#     base_url="https://api.groq.com/openai/v1", # base_url will be your ai's url
+#     api_key=data["key"] # Key will be your api's key
+#     )
     
-response = client.chat.completions.create(
-    model="groq/compound-mini",
-    messages=[{"role": "user", "content": 
-            f"Read the files: {file_list} and return JUST a list with each index being 0-11 for each file depending on their file type based"
-            f"on {[cat["name"] for cat in folderNames["categories"]]}, these files types {[ext["extensions"] for ext in folderNames["categories"]]} are" 
-             "assigned to the same index as the folder indexes, use them to help you sort."}],
-    temperature=0.7
-)
-AI_response = ast.literal_eval(response.choices[0].message.content)
-print(AI_response)
+# response = client.chat.completions.create(
+#     model="groq/compound-mini",
+#     messages=[{"role": "user", "content": 
+#             f"Read the files: {file_list} and return JUST a list with each index being 0-11 for each file depending on their file type based"
+#             f"on {[cat["name"] for cat in folderNames["categories"]]}, these files types {[ext["extensions"] for ext in folderNames["categories"]]} are" 
+#              "assigned to the same index as the folder indexes, use them to help you sort."}],
+#     temperature=0.7
+# )
+# AI_response = ast.literal_eval(response.choices[0].message.content)
+# print(AI_response)
