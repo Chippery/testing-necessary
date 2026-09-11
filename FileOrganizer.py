2f1aff50e694ae1226e3669ac35d5a035ea88c99
Chip <Chippery7@gmail.com>
2026-09-11 01:37:01 -0400

    Ai Sorting

--- FileOrganizer.py
+++ FileOrganizer.py
@@ -1,5 +1,4 @@
 from openai import OpenAI
-from pathlib import Path
 import os 
 import shutil
 import json
@@ -7,10 +6,12 @@ import ast
 
 counter = 0
 create_folder_path = r'c:\Users\chipp_hqhjylc\Downloads' # Change with path you want to clean up
+listdir = os.listdir(create_folder_path)
+file_list = []
 
 with open("key.json", "r") as file:
     data = json.load(file)
-    
+
 with open("fileRecognize.json", "r") as Fold:
     folderNames = json.load(Fold)
 
@@ -19,19 +20,15 @@ for i in folderNames["categories"]: # Attempts to create folders in downloads pa
     os.makedirs(folder_path_name, exist_ok=True)
     counter += 1
 
-listdir = os.listdir(create_folder_path)
-file_list = []
+for eachFile in listdir: # Add files not in directory into list to sort later
+    if os.path.isfile(fr"{create_folder_path}\{eachFile}"):
+        file_list.append(eachFile)
 
-example_src = (fr"{create_folder_path}\{eachFile}") # src = path + file name
-example_dst = (fr"{create_folder_path}\{file_Types[os.path.splitext(eachFile)[1]]}") # dst = path + folder name
 
 client = OpenAI(
     base_url="https://api.groq.com/openai/v1", # base_url will be your ai's url
     api_key=data["key"] # Key will be your api's key
     )
-
-folder_path = r'c:\Users\chipp_hqhjylc\Downloads'
-folder = Path(folder_path)
     
 response = client.chat.completions.create(
     model="groq/compound-mini",
@@ -42,14 +39,12 @@ response = client.chat.completions.create(
     temperature=0.7
 )
 AI_response = ast.literal_eval(response.choices[0].message.content)
-print(AI_response)
 
-try:
-    shutil.move(example_src, example_dst) # Move src to dst
-    print(f"Moved {eachFile} to {file_Types[os.path.splitext(eachFile)[1]]}")
-except shutil.Error as e:
-    if eachFile in os.listdir(example_dst): # Checks if file is in directory folder
-        os.remove(example_src)
-        print(f"Removed {eachFile}, duplicate")
-    else:
-        print(fr"Some stupid file named {eachFile} already exists inside {file_Types[os.path.splitext(eachFile)[1]]}, can't move it.") # Can't and Won't move if file already exists
+for index, item in enumerate(file_list): # Indexing through files not in an folders
+    print(folderNames["categories"][AI_response[index]]["name"])
+    try:
+        shutil.move(fr"{create_folder_path}\{item}", fr"{create_folder_path}\{folderNames["categories"][AI_response[index]]["name"]}") # Move src to dst
+        print(f"Moved {item} to {create_folder_path}\{folderNames["categories"][AI_response[index]]["name"]}")
+    except shutil.Error as e:
+        print(fr"Some stupid file named {item} already exists inside {create_folder_path}\{folderNames["categories"][AI_response[index]]["name"]}, can't move it.") # Can't and Won't move if file already exists
+print(AI_response)
